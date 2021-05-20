6997e3c566708e42a400c89880f05c2372d75349
bbboldemar <bbboldemar@gmail.com>
2021-05-20 07:29:33 +0300

    more or less our GUI

--- GUI.py
+++ GUI.py
@@ -1,42 +1,46 @@
 from tkinter import Button, Entry, Tk, Label, messagebox
+from tkinter.constants import BOTTOM
 import requests
+
+
+
 def change_email_adress(new_email):
     f = open("subscription", "w")
     f.write(new_email)
     f.close()
     messagebox.showinfo ("Success", "Email is changed to " + new_email)
 
-#def create_labels(symbols):
-#    for symbol in symbols:
-#        response = 
-
+ 
 
 root_window = Tk()
-root_window.title("Nani")
-root_window.geometry('480x480')
-
-google_response = requests.get('https://api.twelvedata.com/time_series?symbol=GOOGL&interval=1min&type=stock&outputsize=1&format=JSON&dp=4&timezone=Europe/Moscow&apikey=f7e12a1a4dd34faca920cdff2c088e2b'   )
-
-siacoin_response = requests.get('https://api.twelvedata.com/time_series?symbol=SC/BNB&interval=1min&outputsize=1&format=JSON&dp=4&apikey=f7e12a1a4dd34faca920cdff2c088e2b'    )
-
-#print (google_response)
-#print (google_response.json())
-print (google_response.json()['values'][0]['datetime'])
-print (google_response.json()['values'][0]['close'])
-google_datetime = google_response.json()['values'][0]['datetime']
-google_cost = google_response.json()['values'][0]['close']
+root_window.title("Cryptocurrency Rate")
+root_window.geometry('480x110')
+ 
 
 
-entry = Entry (width=10, borderwidth=1)
-entry.pack()
 
-google_lable = Label(root_window, font=('Arial,25'), text='GOOGL ' + google_datetime + ' ' + google_cost)
-google_lable.pack()#side = LEFT/RIGHT/BOTTOM/TOP
+siacoin_response = requests.get('https://api.twelvedata.com/time_series?symbol=SC/USD&interval=1min&outputsize=3&format=JSON&dp=5&timezone=Europe/Moscow&apikey=f7e12a1a4dd34faca920cdff2c088e2b')
+bitcoin_response = requests.get('https://api.twelvedata.com/time_series?symbol=BTC/USD&interval=1min&outputsize=3&format=JSON&dp=5&timezone=Europe/Moscow&apikey=f7e12a1a4dd34faca920cdff2c088e2b')
 
-apple_lable = Label(root_window, text='SC')
-apple_lable.pack()
+siacoin_datetime = siacoin_response.json()['values'][0]['datetime']
+siacoin_cost = siacoin_response.json()['values'][0]['close']
 
-button = Button(text='Change email adress', command=lambda: change_email_adress(entry.get()))
-button.pack()
+bitcoin_datetime = siacoin_response.json()['values'][0]['datetime']
+bitcoin_cost = bitcoin_response.json()['values'][0]['close']
+
+siacoin_lable = Label(root_window, font=('Arial,25'), text='Siacoin value is ' + siacoin_cost + ' at ' + siacoin_datetime)
+siacoin_lable.pack()#side = LEFT/RIGHT/BOTTOM/TOP
+
+bitcoin_lable = Label(root_window, font=('Arial,25'), text='Bitcoin value is ' + bitcoin_cost + ' at ' + bitcoin_datetime)
+bitcoin_lable.pack()
+
+
+
+button = Button(width=25,text='Change email adress', command=lambda: change_email_adress(entry.get()))
+button.pack(side = BOTTOM)
+entry = Entry (width=25, borderwidth=1)
+entry.pack(side = BOTTOM)
+
+
 
 root_window.mainloop() 
