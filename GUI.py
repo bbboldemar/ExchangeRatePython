6579cc00845bea7ec39e136b7e1aaabe64befea5
bbboldemar <bbboldemar@gmail.com>
2021-05-21 02:38:17 +0300

    added log file

--- GUI.py
+++ GUI.py
@@ -3,6 +3,9 @@ from tkinter.constants import BOTTOM
 import requests
 from datetime import datetime
 from mail_sender import send_email
+import logging
+logging.basicConfig(filename="logfile.log", level=logging.INFO)
+import os
 
 labels = []
 cycles = 0
@@ -11,39 +14,50 @@ prices = [0.02, 39000]
 api_twelvedata = 'https://api.twelvedata.com/time_series?symbol={}/USD&interval=1min&outputsize=3&format=JSON&dp=5&timezone=Europe/Moscow&apikey=f7e12a1a4dd34faca920cdff2c088e2b'
 
 def update_prices():
-    global cycles
-    counter = 0
-    for symbol in symbols:
-        response = requests.get(api_twelvedata.format(symbol))
-        date_time = response.json()['values'][0]['datetime']
-        cost = response.json()['values'][0]['close']
-        if symbol == 'SC':
-            CCoin = 'Siacoin'
-        else:
-            CCoin = 'Bitcoin'
-        if float(cost) > prices[counter]:
-            send_email(CCoin + ' cost is ' + cost + ' at ' + date_time + ' (more than ' + f'{prices[counter]}' + ')')
-            # messagebox.showinfo ("Price reached your limit!", CCoin + ' cost is ' + cost + ' at ' + date_time + ' (more than ' + f'{prices[counter]}' + ')')            
-        labels[counter].configure(text=CCoin + f' ({symbol})' ' value is ' + cost + ' at ' + date_time)
-        counter +=1
-    print ('updated at ' + datetime.today().strftime('%H:%M:%S'))
-    root_window.after(60000, update_prices)
+    try:
+        global cycles
+        counter = 0
+        for symbol in symbols:
+            response = requests.get(api_twelvedata.format(symbol))
+            date_time = response.json()['values'][0]['datetime']
+            cost = response.json()['values'][0]['close']
+            if symbol == 'SC':
+                CCoin = 'Siacoin'
+            else:
+                CCoin = 'Bitcoin'
+            if float(cost) > prices[counter]:
+                send_email(CCoin + ' cost is ' + cost + ' at ' + date_time + ' (more than ' + f'{prices[counter]}' + ')')
+                # messagebox.showinfo ("Price reached your limit!", CCoin + ' cost is ' + cost + ' at ' + date_time + ' (more than ' + f'{prices[counter]}' + ')')         
+            labels[counter].configure(text=CCoin + f' ({symbol})' ' value is ' + cost + ' at ' + date_time)
+            counter +=1
+        logging.info(datetime.today().strftime('%D - %H:%M:%S') + ' Update')
+        root_window.after(60000, update_prices)
+    except:
+        logging.error(datetime.today().strftime('%D - %H:%M:%S') + ' Error: cant reach remote API')
+        messagebox.showinfo ("Error", "Can't reach remote API to update prices")
+        root_window.after(5000, update_prices)        
     
 def create_lables(symbols):
-    for symbol in symbols:
-        response = requests.get(api_twelvedata.format(symbol))
-        date_time = response.json()['values'][0]['datetime']
-        cost = response.json()['values'][0]['close']
-        if symbol == 'SC':
-            CCoin = 'Siacoin'
-        else:
-            CCoin = 'Bitcoin'
-        lable = Label(root_window, font=('Arial,25'), text=CCoin + f' ({symbol})' ' value is ' + cost + ' at ' + date_time)
-        lable.pack()
-        labels.append(lable)
+    try:
+        for symbol in symbols:
+            response = requests.get(api_twelvedata.format(symbol))
+            date_time = response.json()['values'][0]['datetime']
+            cost = response.json()['values'][0]['close']
+            if symbol == 'SC':
+                CCoin = 'Siacoin'
+            else:
+                CCoin = 'Bitcoin'
+            lable = Label(root_window, font=('Arial,25'), text=CCoin + f' ({symbol})' ' value is ' + cost + ' at ' + date_time)
+            lable.pack()
+            labels.append(lable)
+    except:
+        logging.error(datetime.today().strftime('%D - %H:%M:%S') + ' Error: cant reach remote API')
+        messagebox.showinfo ("Error", "Can't reach remote server")
+        os._exit(os.EX_OK)
+
 
 def change_email_adress(new_email):
-    f = open("exchanger_setting", "w")
+    f = open("exchanger_settings", "w")
     f.write(new_email)
     f.close()
     messagebox.showinfo ("Success", "Email is changed to " + new_email)
@@ -59,5 +73,6 @@ button.pack(side = BOTTOM)
 entry = Entry(width=25, borderwidth=1)
 entry.pack(side = BOTTOM)
 
-root_window.after(10000, update_prices)
-root_window.mainloop() 
+root_window.after(5000, update_prices)
+logging.info(datetime.today().strftime('%D - %H:%M:%S') + ' Start')
+root_window.mainloop()
--- mail_sender.py
+++ mail_sender.py
@@ -1,55 +1,49 @@
 import smtplib
-import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from tkinter import messagebox
+from datetime import datetime
+import logging
+logging.basicConfig(filename="logfile.log", level=logging.INFO)
 
-def get_address():
-    # f = open("exchanger_setting", "r")
+def get_email_data():
+    # f = open("exchanger_settings", "r")
     # contents = f.read()
     # f.close()
     # return contents
-    return '******@gmail.com'
+    return '******@gmail.com', '******'
 
-def get_password():
-    return '******'
-
-# your_address = 'your_address@gmail.com'
-# your_password = 'your_password'
-# 2-step verification must be disabled, less secure apps must be enabled
 def send_email(mail_content):
     try:
-        your_address = get_address()
+        your_address, your_password = get_email_data()
+        exchanger_setting_exists = True
     except:
-        messagebox.showinfo ("Error", "Please input valid email adress")
-        os._exit(os.EX_OK)
-        # quit()
-        # exit()
-        # sys.exit('Error via sending email: address')
-    try:
-        your_password = get_password()
-    except:
-        messagebox.showinfo ("Error", "Please input valid password")
-        os._exit(os.EX_OK)
-        # sys.exit('Error via sending email: password')    
-    try:
-        message = MIMEMultipart()
-        message['From'] = your_address
-        message['To'] = your_address
-        message['Subject'] = 'Price update from your app'
-        message.attach(MIMEText(mail_content, 'plain'))
-        session = smtplib.SMTP('smtp.gmail.com', 587)
-        # must works with yandex.ru with another address and port
-        # doesn't work with mail.ru at all
-        session.starttls()
-        session.login(your_address, your_password)
-        text = message.as_string()
-        session.sendmail(your_address, your_address, text)
-        session.quit()
-        print('Mail sent')
-    except:
-        messagebox.showinfo ("Error", "Invalid email address or password")
-        print('Error via sending email: invalid SMTP parameters')
+        logging.error(datetime.today().strftime('%D - %H:%M:%S') + ' Error via sending email: cant reach exchanger_settings')
+        exchanger_setting_exists = False
+        # messagebox.showinfo ("Error", "Can't reach Subscription Setting ")
+    if  exchanger_setting_exists == True:
+        try:
+            message = MIMEMultipart()
+            message['From'] = your_address
+            message['To'] = your_address
+            message['Subject'] = 'Price update from your app'
+            message.attach(MIMEText(mail_content, 'plain'))
+            session = smtplib.SMTP('smtp.gmail.com', 587)
+            session.starttls()
+            session.login(your_address, your_password)
+            text = message.as_string()
+            session.sendmail(your_address, your_address, text)
+            session.quit()
+            logging.info(datetime.today().strftime('%D - %H:%M:%S') + ' Mail sent')
+        except:
+            logging.error(datetime.today().strftime('%D - %H:%M:%S') + ' Error via sending email: invalid SMTP parameters')
+            # messagebox.showinfo ("Error", "Invalid email address or password")
 
 
 # send_email('OwO')
+
+# must works with yandex.ru with another address and port
+# doesn't work with mail.ru at all
+# your_address = 'your_address@gmail.com'
+# your_password = 'your_password'
+# 2-step verification must be disabled, less secure apps must be enabled
