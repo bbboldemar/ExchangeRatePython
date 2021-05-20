2d1f5c81e8ec2a49ac718812e1fb8e5c26ae788a
bbboldemar <bbboldemar@gmail.com>
2021-05-20 09:43:04 +0300

    adress and pass through code

--- GUI.py
+++ GUI.py
@@ -1,54 +1,52 @@
 from tkinter import Button, Entry, Tk, Label, messagebox
 from tkinter.constants import BOTTOM
 import requests
+from datetime import datetime
+from mail_sender import send_email
 
 labels = []
 cycles = 0
 symbols = ['SC','BTC']
+prices = [0.02, 39000]
+api_twelvedata = 'https://api.twelvedata.com/time_series?symbol={}/USD&interval=1min&outputsize=3&format=JSON&dp=5&timezone=Europe/Moscow&apikey=f7e12a1a4dd34faca920cdff2c088e2b'
 
 def update_prices():
     global cycles
     counter = 0
-    print ('update')
     for symbol in symbols:
-        response = requests.get(
-            f'https://api.twelvedata.com/time_series?symbol={symbol}/USD&interval=1min&outputsize=3&format=JSON&'
-            f'dp=5&timezone=Europe/Moscow&apikey=f7e12a1a4dd34faca920cdff2c088e2b'
-            )
-        datetime = response.json()['values'][0]['datetime']
+        response = requests.get(api_twelvedata.format(symbol))
+        date_time = response.json()['values'][0]['datetime']
         cost = response.json()['values'][0]['close']
         if symbol == 'SC':
             CCoin = 'Siacoin'
         else:
             CCoin = 'Bitcoin'
-        labels[counter].configure(text=CCoin + f' ({symbol})' ' value is ' + cost + ' at ' + datetime)
+        if float(cost) > prices[counter]:
+            send_email(CCoin + ' cost is ' + cost + ' at ' + date_time + ' (more than ' + f'{prices[counter]}' + ')')
+            # messagebox.showinfo ("Price reached your limit!", CCoin + ' cost is ' + cost + ' at ' + date_time + ' (more than ' + f'{prices[counter]}' + ')')            
+        labels[counter].configure(text=CCoin + f' ({symbol})' ' value is ' + cost + ' at ' + date_time)
         counter +=1
+    print ('updated at ' + datetime.today().strftime('%H:%M:%S'))
     root_window.after(60000, update_prices)
     
-
 def create_lables(symbols):
     for symbol in symbols:
-        response = requests.get(
-            f'https://api.twelvedata.com/time_series?symbol={symbol}/USD&interval=1min&outputsize=3&format=JSON&'
-            f'dp=5&timezone=Europe/Moscow&apikey=f7e12a1a4dd34faca920cdff2c088e2b'
-            )
-        datetime = response.json()['values'][0]['datetime']
+        response = requests.get(api_twelvedata.format(symbol))
+        date_time = response.json()['values'][0]['datetime']
         cost = response.json()['values'][0]['close']
         if symbol == 'SC':
             CCoin = 'Siacoin'
         else:
             CCoin = 'Bitcoin'
-        lable = Label(root_window, font=('Arial,25'), text=CCoin + f' ({symbol})' ' value is ' + cost + ' at ' + datetime)
+        lable = Label(root_window, font=('Arial,25'), text=CCoin + f' ({symbol})' ' value is ' + cost + ' at ' + date_time)
         lable.pack()
         labels.append(lable)
 
-
 def change_email_adress(new_email):
-    f = open("subscription", "w")
+    f = open("exchanger_setting", "w")
     f.write(new_email)
     f.close()
     messagebox.showinfo ("Success", "Email is changed to " + new_email)
-
 
 root_window = Tk()
 root_window.title("Cryptocurrency to USD")
@@ -58,8 +56,8 @@ create_lables(symbols)
 
 button = Button(width=25,text='Change email adress', command=lambda: change_email_adress(entry.get()))
 button.pack(side = BOTTOM)
-entry = Entry (width=25, borderwidth=1)
+entry = Entry(width=25, borderwidth=1)
 entry.pack(side = BOTTOM)
 
-root_window.after(60000, update_prices)
+root_window.after(10000, update_prices)
 root_window.mainloop() 
--- mail_sender.py
+++ mail_sender.py
@@ -1,26 +1,55 @@
 import smtplib
+import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import os
+from tkinter import messagebox
 
-# email_address = 'your_address@gmail.com'
-# email_password = 'your_password'
+def get_address():
+    # f = open("exchanger_setting", "r")
+    # contents = f.read()
+    # f.close()
+    # return contents
+    return '******@gmail.com'
+
+def get_password():
+    return '******'
+
+# your_address = 'your_address@gmail.com'
+# your_password = 'your_password'
 # 2-step verification must be disabled, less secure apps must be enabled
-
 def send_email(mail_content):
-    message = MIMEMultipart()
-    message['From'] = email_address
-    message['To'] = email_address
-    message['Subject'] = 'Price update from your app'
-    message.attach(MIMEText(mail_content, 'plain'))
-    session = smtplib.SMTP('smtp.gmail.com', 587) 
-    # must works with yandex.ru with another address and port
-    # doesn't work with mail.ru at all
-    session.starttls()
-    session.login(email_address, email_password)
-    text = message.as_string()
-    session.sendmail(email_address, email_address, text)
-    session.quit()
-    print('Mail sent')
+    try:
+        your_address = get_address()
+    except:
+        messagebox.showinfo ("Error", "Please input valid email adress")
+        os._exit(os.EX_OK)
+        # quit()
+        # exit()
+        # sys.exit('Error via sending email: address')
+    try:
+        your_password = get_password()
+    except:
+        messagebox.showinfo ("Error", "Please input valid password")
+        os._exit(os.EX_OK)
+        # sys.exit('Error via sending email: password')    
+    try:
+        message = MIMEMultipart()
+        message['From'] = your_address
+        message['To'] = your_address
+        message['Subject'] = 'Price update from your app'
+        message.attach(MIMEText(mail_content, 'plain'))
+        session = smtplib.SMTP('smtp.gmail.com', 587)
+        # must works with yandex.ru with another address and port
+        # doesn't work with mail.ru at all
+        session.starttls()
+        session.login(your_address, your_password)
+        text = message.as_string()
+        session.sendmail(your_address, your_address, text)
+        session.quit()
+        print('Mail sent')
+    except:
+        messagebox.showinfo ("Error", "Invalid email address or password")
+        print('Error via sending email: invalid SMTP parameters')
+
 
 # send_email('OwO')
