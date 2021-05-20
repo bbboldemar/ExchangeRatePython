2a3d3308f598b3dfe5f1b7d68ac4649ee394d0ef
bbboldemar <bbboldemar@gmail.com>
2021-05-20 08:23:59 +0300

    realtime update

--- GUI.py
+++ GUI.py
@@ -2,6 +2,29 @@ from tkinter import Button, Entry, Tk, Label, messagebox
 from tkinter.constants import BOTTOM
 import requests
 
+labels = []
+cycles = 0
+symbols = ['SC','BTC']
+
+def update_prices():
+    global cycles
+    counter = 0
+    print ('update')
+    for symbol in symbols:
+        response = requests.get(
+            f'https://api.twelvedata.com/time_series?symbol={symbol}/USD&interval=1min&outputsize=3&format=JSON&'
+            f'dp=5&timezone=Europe/Moscow&apikey=f7e12a1a4dd34faca920cdff2c088e2b'
+            )
+        datetime = response.json()['values'][0]['datetime']
+        cost = response.json()['values'][0]['close']
+        if symbol == 'SC':
+            CCoin = 'Siacoin'
+        else:
+            CCoin = 'Bitcoin'
+        labels[counter].configure(text=CCoin + f' ({symbol})' ' value is ' + cost + ' at ' + datetime)
+        counter +=1
+    root_window.after(60000, update_prices)
+    
 
 def create_lables(symbols):
     for symbol in symbols:
@@ -12,11 +35,13 @@ def create_lables(symbols):
         datetime = response.json()['values'][0]['datetime']
         cost = response.json()['values'][0]['close']
         if symbol == 'SC':
-            CC = 'Siacoin'
+            CCoin = 'Siacoin'
         else:
-            CC = 'Bitcoin'
-        lable = Label(root_window, font=('Arial,25'), text=CC + f' ({symbol})' ' value is ' + cost + ' at ' + datetime)
+            CCoin = 'Bitcoin'
+        lable = Label(root_window, font=('Arial,25'), text=CCoin + f' ({symbol})' ' value is ' + cost + ' at ' + datetime)
         lable.pack()
+        labels.append(lable)
+
 
 def change_email_adress(new_email):
     f = open("subscription", "w")
@@ -24,11 +49,11 @@ def change_email_adress(new_email):
     f.close()
     messagebox.showinfo ("Success", "Email is changed to " + new_email)
 
+
 root_window = Tk()
-root_window.title("Cryptocurrency Rate")
+root_window.title("Cryptocurrency to USD")
 root_window.geometry('500x125')
 
-symbols = ['SC','BTC']
 create_lables(symbols)
 
 button = Button(width=25,text='Change email adress', command=lambda: change_email_adress(entry.get()))
@@ -36,5 +61,5 @@ button.pack(side = BOTTOM)
 entry = Entry (width=25, borderwidth=1)
 entry.pack(side = BOTTOM)
 
-
+root_window.after(60000, update_prices)
 root_window.mainloop() 
