fd57a68bc4b403c861af472ffb775f7c27f39098
Vladimir Kosyrev <80014540+bbboldemar@users.noreply.github.com>
2021-10-19 02:44:04 +0300

    Revert "Work in progress..."

--- API.py
+++
@@ -1,3 +0,0 @@
-api_twelvedata = 'https://api.twelvedata.com/time_series?symbol={}/USD&interval=1min&outputsize=3&format=JSON&dp=5&timezone=Europe/Moscow&apikey=ab2a1285e34c4fa78173db8c5a9f6d5f'
-# key=f7e12a1a4dd34faca920cdff2c088e2b
-# key=ab2a1285e34c4fa78173db8c5a9f6d5f
